91bb6af6b697364c3f38322ba45cc14338d015bc
Dave Kuhlman <dkuhlman@davekuhlman.org>
2020-11-06 16:47:02 -0800

    Added test01*.lua; updated transformlib.lua.

--- transformlib.lua
+++ transformlib.lua
@@ -4,7 +4,8 @@ local Module = {}
 
 function Module.convert(filenames)
   for idx, filename in ipairs(filenames) do
-    print(string.format('%3d. converting: "%s"', idx, filename))
+    --print(string.format('%3d. converting: "%s"', idx, filename))
+    print(string.format('%3d. converting: asciidoctor %s', idx, filename))
     local cmd = string.format('asciidoctor %s', filename)
     --print(string.format('cmd: "%s"', cmd))
     io.popen(cmd)
